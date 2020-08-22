8a8b40d7efa8be08f3f0f8035dc34d436d865130
Edgar Zea <cesszea@icloud.com>
2020-08-22 14:25:44 -0500

    Gif Expert App with React done :tada:

---
+++ src/components/AddCategory.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const AddCategory = ({ setCategories }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setInputValue(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (inputValue.trim().length > 1) {
+      setCategories((cats) => [inputValue, ...cats]);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleChange}
+        placeholder="Pikachu"
+      />
+    </form>
+  );
+};
+
+AddCategory.propTypes = {
+  setCategories: PropTypes.func.isRequired,
+};
+
+export default AddCategory;
